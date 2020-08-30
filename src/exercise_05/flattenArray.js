e1211eaeb806989d17f12afa9f288eb5de7355c5
hongyue-qiu <994059641@qq.com>
2020-08-30 11:25:58 +0800

    [hongyue-qiu fix= pix accessing property test.

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,16 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if(array == null) throw 'Flatten undefined or null array';
+  var newArray = [];
+  for(var i =0; i < array.length; i++){
+    if(array[i].length>0){
+      for(var j=0; j < array[i].length; j++){
+        newArray.push(array[i][j]);
+      }
+    }else{
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
 }
